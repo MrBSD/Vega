76c0d0f04b2d58c3e6784df8ae4ee46f2bc7604a
MrBSD <MrBSD82@gmail.com>
2017-09-19 16:48:35 +0300

    added Vehicle entity class

---
+++ Models/Vehicle.cs
@@ -0,0 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Vega.Models
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+        public int ModelId { get; set; }
+        public Model Model { get; set; }
+        public bool IsRegistered { get; set; }
+        
+        [Required]
+        [MaxLength(255)]
+        public string ContactName { get; set; }
+        
+        [MaxLength(255)]
+        public string ContactEmail { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string ContactPhone { get; set; }
+        public DateTime LastUpdate { get; set; }
+    }
+}
