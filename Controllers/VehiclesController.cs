bea157370643d70942e181d6eccf70d01fb32a1e
MrBSD <MrBSD82@gmail.com>
2017-09-20 12:34:19 +0300

    Implemented API for getting a vehicle

--- Controllers/VehiclesController.cs
+++ Controllers/VehiclesController.cs
@@ -36,6 +36,22 @@ namespace Vega.Controllers
             return Ok(result);
         }
 
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetVehicle(int id)
+        {
+            var vehicle = await context.Vehicles
+            .Include(v => v.Features)
+            .SingleOrDefaultAsync (v =>v.Id==id);
+
+            if (vehicle==null)
+            {
+                return NotFound();
+            }
+            
+            var vehicleResource = mapper.Map<Vehicle,VehicleResource>(vehicle);
+            return Ok(vehicleResource);
+        }
+
           
         [HttpPost("{id}")]
         public async Task<IActionResult> UpdateVehicle(int id, [FromBody]VehicleResource vehicleResource)
