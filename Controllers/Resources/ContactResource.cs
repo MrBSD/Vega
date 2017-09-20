88a4701448bf6ee104703f992deb5dcd6a0c5f1a
MrBSD <MrBSD82@gmail.com>
2017-09-20 07:54:02 +0300

    Added Vehicle and Contact API Resources

---
+++ Controllers/Resources/ContactResource.cs
@@ -0,0 +1,16 @@
+namespace Vega.Controllers.Resources
+{
+    public class ContactResource
+    {
+        [Required]
+        [MaxLength(255)]
+        public string Name { get; set; }
+
+        [MaxLength(255)]
+        public string Email { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Phone { get; set; }
+    }
+}
